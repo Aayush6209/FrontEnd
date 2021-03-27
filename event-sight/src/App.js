c4bf2843dd8bc259246946984bd0bf03d4a82a5e
shivam-arora-13 <shivamarora.bt19cse@pec.edu.in>
2021-03-27 10:13:54 +0530

    good to go

--- event-sight/src/App.js
+++ event-sight/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Jumbotron} from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+  return <>
+  <Jumbotron>
+  <h1 className="display-3">Event-Sight</h1>
+  <p>Welcome to front end</p>
+  </Jumbotron></>;
 }
 
 export default App;
