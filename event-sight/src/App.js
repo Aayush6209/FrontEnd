f57d0fe8c03b7e0990ea98f4f6bb5ae044f1721f
shivam-arora-13 <shivamarora.bt19cse@pec.edu.in>
2021-03-28 09:34:24 +0530

    example removed

--- event-sight/src/App.js
+++ event-sight/src/App.js
@@ -5,11 +5,7 @@ import SignUp from "./components/signup";
 import Login from "./components/login";
 import EventForm from "./components/eventForm"
 import ESNavbar from "./components/esNavbar";
-import Example from "./components/EventCard";
-<<<<<<< HEAD
 import Sidebar from "./components/sidebar";
-=======
->>>>>>> 97a1e3bb2f73211bf53b53af2283dba8bae0f875
 
 const App = ()=>{
   return <div>
@@ -25,13 +21,8 @@ const App = ()=>{
   <SignUp/>
   <Login/>
   <EventForm/>
-  <Example />
-<<<<<<< HEAD
   </div>
   </div>;
-=======
-  </>;
->>>>>>> 97a1e3bb2f73211bf53b53af2283dba8bae0f875
 }
 
 export default App;
