23720a2c4991200f9407280550e201a9d6bdd420
shivam-arora-13 <shivamarora.bt19cse@pec.edu.in>
2021-03-31 14:04:07 +0530

    made changes in sidebar

--- event-sight/src/components/sidebar.jsx
+++ event-sight/src/components/sidebar.jsx
@@ -7,20 +7,17 @@ import {OCLogos} from "../assets/OClogo";
 
 let logos = [];
 for(let key in OCLogos){
-    logos.push(<Col key={key} xs="12"><div>
+    logos.push( <div key={key} style={{
+        display : "inline-block"
+    }}>
        <Link to="/oc-page" ><img className="SidebarCard" src = "https://images.jdmagicbox.com/comp/chandigarh/c4/0172p1762.1762.110201201416.l1c4/catalogue/punjab-engineering-college-sector-12-chandigarh-placement-services-candidate--3er9sw3.jpg?clr=#006600" alt="pec"/>
-       </Link></div></Col>)
+       </Link></div>)
 }
 
 
 const Sidebar = ()=>{
     return <div className="Sidebar">
-        <center>
-        <Row> 
         {logos.map((logo)=>logo)} 
-        <Col xs="12" style={{height : "80px"}}> <div> </div></Col>
-        </Row>
-        </center>
     </div>
 }
 
