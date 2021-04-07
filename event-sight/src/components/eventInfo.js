eca4e162fe28e405d7739501a266bc01ce71ad60
Arjun Kathail <arjunkathail5@gmail.com>
2021-04-07 13:31:57 +0530

    Customize Event Info Add XSS Security

--- event-sight/src/components/eventInfo.js
+++ event-sight/src/components/eventInfo.js
@@ -5,24 +5,38 @@ import classnames from 'classnames';
 import Sample from "../assets/sample.jpg";
 import {SampleEvent} from "../assets/sampleEvent";
 
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
 const EventInfo = ()=>{
     
     //to toggle the tabs
     const [activeTab, setActiveTab] = useState('1');
     const toggle = tab => {if(activeTab !== tab) setActiveTab(tab);}
 
+    function linkify(text) {
+        var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        return text.replace(urlRegex, function(url) {
+            return '<a style="text-decoration:none;" target="_blank" href="' + url + '">' + url + '</a>';
+        });
+    }
 
     return <div className="EventInfoDiv">
         <Nav tabs>
         <NavItem>
           <NavLink
             className={classnames({ active: activeTab === '1' })} onClick={() => { toggle('1'); }}>
-                Event Poster
+                <span style={{cursor : "pointer"}}>Event Poster</span>
           </NavLink></NavItem>
         <NavItem>
           <NavLink
             className={classnames({ active: activeTab === '2' })} onClick={() => { toggle('2'); }}>
-            Detailed Info</NavLink></NavItem>
+            <span style={{cursor : "pointer"}}>Detailed Info</span>
+          </NavLink>
+        </NavItem>
       </Nav>
 
       <TabContent activeTab={activeTab}>
@@ -40,13 +54,14 @@ const EventInfo = ()=>{
             {/* Second Tab Content */}
             <h1 className="EventTitle">{SampleEvent.eventTitle}</h1>
             <div className="EventInfo">
-            <p  className="EventInfoElement">{SampleEvent.eventDescription}</p>
+            <p  className="EventInfoIntro">{SampleEvent.eventDescription}</p>
             <Row>
-                <Col lg="6" className="EventInfoElement">Date : {SampleEvent.eventDate}</Col>
-                <Col lg="6" className="EventInfoElement">Time : {SampleEvent.eventTime}</Col>
+                <Col lg="12" className="EventInfoDate">Date: <span className="EventDateTime">{SampleEvent.eventDate}</span></Col>
+                <Col lg="12" className="EventInfoTime">Time: <span className="EventDateTime">{SampleEvent.eventTime}</span></Col>
             </Row>
             {SampleEvent.eventDetails.map((detail, index)=>{
-                return <p key={index} className="EventInfoElement">{detail}</p>
+                let clean = DOMPurify.sanitize(detail);
+                return <p key={index} className="EventInfoDetail" dangerouslySetInnerHTML={{__html: linkify(clean)}} />
             })}
             </div>
         </TabPane>
