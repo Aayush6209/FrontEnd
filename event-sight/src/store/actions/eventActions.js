f8f2b8d2dc5220dec7c483b402fa1d9043fb7f4a
Arjun Kathail <arjunkathail5@gmail.com>
2021-04-13 21:36:41 +0530

    Add Redux State Display_Registered_Events

--- event-sight/src/store/actions/eventActions.js
+++ event-sight/src/store/actions/eventActions.js
@@ -14,3 +14,37 @@ export const createNewEvent = (event)=>{
     }
 }
 
+export const displayRegisteredEvents = ()=>{
+    return dispatch =>{
+        dispatch(displayRegisteredEventsReq)
+        axios
+        .get('')
+        .then(res => {
+            const events = res.data
+            dispatch(displayRegisteredEventsSuccess(events))
+        })
+        .catch(error => {
+            dispatch(displayRegisteredEventsFailure(error))
+        })
+    }
+}
+
+export const displayRegisteredEventsReq = ()=>{
+    return {
+        type: actionTypes.DISPLAY_REGISTERED_EVENTS_REQ,
+    }
+}
+
+export const displayRegisteredEventsSuccess = events=>{
+    return {
+        type: actionTypes.DISPLAY_REGISTERED_EVENTS_SUCCESS,
+        payload: events
+    }
+}
+
+export const displayRegisteredEventsFailure = error=>{
+    return {
+        type: actionTypes.DISPLAY_REGISTERED_EVENTS_FAILURE,
+        payload: error
+    }
+}
--- event-sight/src/store/reducers/eventReducer.js
+++ event-sight/src/store/reducers/eventReducer.js
@@ -1,12 +1,29 @@
+import * as actionTypes from "../actions/actionTypes";
+
 const initialState = {
-    showAlert : false
+  showAlert: false,
 };
 
-const EventReducer = (state = initialState, action)=>{
-    switch(action.type){
-        default : return state;
-    }
-    
-}
+const EventReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.DISPLAY_REGISTERED_EVENTS_REQ:
+      return {
+        ...state,
+        showAlert: true,
+      };
+    case actionTypes.DISPLAY_REGISTERED_EVENTS_REQ_SUCCESS:
+      return {
+        ...state,
+        showAlert: true,
+      };
+    case actionTypes.DISPLAY_REGISTERED_EVENTS_REQ_FAILURE:
+      return {
+        ...state,
+        showAlert: true,
+      };
+    default:
+      return state;
+  }
+};
 
 export default EventReducer;
