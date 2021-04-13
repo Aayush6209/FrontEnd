86c4768829a6829783dea634a71409c5060ba69c
Arjun Kathail <arjunkathail5@gmail.com>
2021-04-13 09:54:31 +0530

    OC page edited

--- event-sight/src/container/OCPage.js
+++ event-sight/src/container/OCPage.js
@@ -1,46 +1,66 @@
 import React from "react";
-import {Row, Col, Button} from "reactstrap";
+import { Row, Col, Button } from "reactstrap";
 import EventCard from "../components/EventCard";
-import {MdCardMembership} from "react-icons/md";
-import {AiOutlineUserAdd} from "react-icons/ai";
+import { MdCardMembership } from "react-icons/md";
+import { AiOutlineUserAdd } from "react-icons/ai";
 
-const OCPage = ()=>{
-    const imgURL = "https://images.jdmagicbox.com/comp/chandigarh/c4/0172p1762.1762.110201201416.l1c4/catalogue/punjab-engineering-college-sector-12-chandigarh-placement-services-candidate--3er9sw3.jpg?clr=#006600";
-    //for testing only
-    var foo = [];
-    for (var i = 1; i <= 5; i++) {foo.push(i);}
-    
-    return <>
-    <div className="OCPageHeader">
+const OCPage = () => {
+  const imgURL =
+    "https://images.jdmagicbox.com/comp/chandigarh/c4/0172p1762.1762.110201201416.l1c4/catalogue/punjab-engineering-college-sector-12-chandigarh-placement-services-candidate--3er9sw3.jpg?clr=#006600";
+  //for testing only
+  var foo = [];
+  for (var i = 1; i <= 5; i++) {
+    foo.push(i);
+  }
+
+  return (
+    <>
+      <div className="OCPageHeader">
         <Row>
-            <Col lg="3"><img src={imgURL} alt="pec" className="OCPageHeaderImg"/></Col>
-            <Col lg="9">
+          <Col lg="3" md="3" sm="3">
+            <img src={imgURL} alt="pec" className="OCPageHeaderImg" />
+          </Col>
+          <Col lg="6" md="6" sm="6">
             <Row className="OCPageHeaderR1">
-                <Col lg="7"><h1 className="OCPageHeaderText">Punjab Engineering College</h1></Col>
-                <Col lg="5">
-            <Button outline color="primary" className="OCPageButtons">Follow <AiOutlineUserAdd size="25px"/></Button>
-            <Button outline color="danger" className="OCPageButtons">Request Membership <MdCardMembership size="25px"/></Button>
-            </Col>
+              <Col>
+                <h1 className="OCPageHeaderText">Punjab Engineering College</h1>
+              </Col>
             </Row>
             <Row className="OCPageHeaderR2">
-                <Col>{251} Followers</Col>
-                <Col>{153} Members</Col>
+              <Col lg="6" md="6" sm="6">
+                <div className="Followers">{251} Followers</div>
+              </Col>
+              <Col lg="6" md="6" sm="6">
+                <div className="Members">{153} Members</div>
+              </Col>
             </Row>
             <Row className="OCPageHeaderR3">
-                <h5>{"PEC : A pioneer engineering college"}</h5>
+              <h5>{"PEC : A pioneer engineering college"}</h5>
             </Row>
-            </Col>
-        </Row>    
-    </div>
-    <div className="OCPageBody">
+          </Col>
+          <Col lg="3" md="3" sm="3">
+            <div className="Follow">
+              <Button outline color="primary" className="OCPageButtons">
+                Follow <AiOutlineUserAdd size="25px" />
+              </Button>
+            </div>
+            <Button outline color="danger" className="OCPageButtons">
+              Request Membership <MdCardMembership size="25px" />
+            </Button>
+          </Col>
+        </Row>
+      </div>
+      <div className="OCPageBody">
         <Row lg="1" md="1" sm="1" xs="1">
-    {
-        foo.map(k=><Col key={k} ><EventCard/></Col>)
-    }
-    </Row>
-    </div>
-
+          {foo.map((k) => (
+            <Col key={k}>
+              <EventCard />
+            </Col>
+          ))}
+        </Row>
+      </div>
     </>
-}
+  );
+};
 
 export default OCPage;
