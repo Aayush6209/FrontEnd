88354344b6b25103223354f6f8c54053e45283fe
shivam-arora-13 <shivamarora.bt19cse@pec.edu.in>
2021-04-13 11:03:50 +0530

    .

--- event-sight/src/assets/URLImages.js
+++ event-sight/src/assets/URLImages.js
@@ -1,1 +1,1 @@
-export const Img = {img: "https://venngage-wordpress.s3.amazonaws.com/uploads/2018/11/Blue-Conservation-Creative-Poster-Example-Template.jpg"};
+export const Img = {img: "https://content3.jdmagicbox.com/comp/chandigarh/c4/0172p1762.1762.110201201416.l1c4/catalogue/punjab-engineering-college-sector-12-chandigarh-colleges-9xhdnwx78q.jpg"};
