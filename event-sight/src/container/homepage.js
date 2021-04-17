10fc0da80d4ea72a9b11f96a0d4c7c0fb493769e
Arjun Kathail <arjunkathail5@gmail.com>
2021-04-17 14:22:44 +0530

    Display Events

--- event-sight/src/container/homepage.js
+++ event-sight/src/container/homepage.js
@@ -1,23 +1,50 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Row, Col} from "reactstrap";
-
 import EventCard from "../components/EventCard";
 import Sidebar from "../components/sidebar";
 import {Img} from "../assets/URLImages";
+import {Link} from "react-router-dom";
+import ESSpinner from "../UI/ESSpinner";
+import {connect} from "react-redux";
+import * as eventActions from "../store/actions/eventActions";
+import * as actionTypes from "../store/actions/actionTypes";
 
-const Homepage = ()=>{
-    //testing
-    var foo = [];
-    for (var i = 1; i <= 20; i++) {foo.push(i);}
-    return <>
-    <Sidebar/>
-    <Row lg="2" md="2" sm="1" xs="1">
-    {
-        foo.map(k=><Col key={k}><EventCard img={Img.img}/></Col>)
+const Homepage = (props)=>{
+
+    useEffect(()=>{
+        props.displayEvents()
+    }, [])
+    let eventsrender = null;
+    var i=0;
+    if(props.loading){
+        eventsrender = <ESSpinner />
     }
-    </Row>
+    else{
+        if(Homepage !== null){
+            eventsrender=props.events && <Row lg="2" md="2" sm="1" xs="1">
+            {
+                props.events.map((event, index)=><Col key={index}><EventCard img={Img.img}/></Col>)
+            }
+            </Row>}
+    }
+    return <>
+    <Sidebar />
+    {eventsrender}
     </>
 
 }
 
-export default Homepage;
+const mapStateToProps = (state)=>{
+    return {
+        loading: state.event.loading,
+        events: state.event.events,
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        displayEvents : ()=>dispatch(eventActions.displayEvents()),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Homepage);
